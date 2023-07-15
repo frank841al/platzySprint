986c69bc85571a1770881bafa24c96cc3416ce61
frank841al <frank.841.al@gmail.com>
2023-07-15 10:29:34 -0500

    primera sersion

---
+++ src/main/java/com/platzii/makita/PlatziMakitaApplication.java
@@ -0,0 +1,13 @@
+package com.platzii.makita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMakitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMakitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platzii/makita/PlatziMakitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzii.makita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziMakitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
